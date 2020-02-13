255a4bd228ed60d6cfc0f9a41aca08346d6a6d50
gbili <accounts.guillermo@gmail.com>
2020-02-13 21:49:48 +0100

    feat: prepare terrain for ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,8 @@
 import MysqlReq from './MysqlReq';
-import MysqlInstantiatableReq from './MysqlInstantiatableReq';
 import ActionResult from './ActionResult';
 import MysqlDump from './MysqlDump';
 import RequestorCapability from './models/RequestorCapability';
 import RequestorModel from './models/RequestorModel';
 
-export { MysqlReq, MysqlDump, MysqlInstantiatableReq, ActionResult, RequestorCapability, RequestorModel };
+export { MysqlReq, MysqlDump, ActionResult, RequestorCapability, RequestorModel };
 export default MysqlInstantiatableReq;
