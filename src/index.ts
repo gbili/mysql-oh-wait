1c4fd64c825b2588bf7c3c28c76465abd2a03754
Guillermo Pages <mail@guillermo.at>
2025-02-24 22:21:23 +0100

    fix: not exporting

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,8 @@ import { ActionResult, OkPacket } from './ActionResult';
 import RequestorCapability from './models/RequestorCapability';
 import RequestorModel from './models/RequestorModel';
 import QueryFormat from './QueryFormat';
+import { extractConfigFromEnv, isMissingConfigProps, type UserProvidedEnvVarNames, type ConfigPropsOptional } from './config';
 
-export type { ActionResult, OkPacket }
-export { QueryFormat, MysqlReq, RequestorCapability, RequestorModel };
+export type { ActionResult, OkPacket, UserProvidedEnvVarNames, ConfigPropsOptional }
+export { QueryFormat, MysqlReq, RequestorCapability, RequestorModel, extractConfigFromEnv, isMissingConfigProps };
 export default MysqlReq;
